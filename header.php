863f77125610d8153ea2401ff175d2ef535b0b48
r-lodahl <robert.lodahl@med.uni-goettingen.de>
2018-05-15 15:23:02 +0200

    first commit

---
+++ header.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html class="no-js" <?php language_attributes(); ?>>
+<head>
+  <meta name="description" content="<?php if ( is_single() ) {
+      single_post_title('', true);
+    } else {
+      bloginfo('name'); echo " - "; bloginfo('description');
+    }
+  ?>" />
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+
+<nav class="navbar navbar-expand-md navbar-light bg-light">
+  <div class="container-responsive">
+
+    <a class="navbar-brand" href="<?php echo esc_url( home_url('/') ); ?>"><?php bloginfo('name'); ?></a>
+
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarDropdown" aria-controls="navbarDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarDropdown">
+      <?php
+        wp_nav_menu( array(
+          'theme_location'  => 'navbar',
+          'container'       => false,
+          'menu_class'      => '',
+          'fallback_cb'     => '__return_false',
+          'items_wrap'      => '<ul id="%1$s" class="navbar-nav mr-auto mt-2 mt-lg-0 %2$s">%3$s</ul>',
+          'depth'           => 2,
+          'walker'          => new illarion_walker_nav_menu()
+        ) );
+      ?>
+
+    </div>
+
+  </div>
+</nav>
